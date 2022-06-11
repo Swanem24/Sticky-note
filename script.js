afefed357f05feaff155c1b02a8a02881f2c3294
Sanjog Wanem <37484722+Swanem24@users.noreply.github.com>
2022-06-11 15:09:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,33 @@
-const quizData = [
-    {
-        question: "What is 2 + 2?",
-        a: "4",
-        b: "1",
-        c: "2",
-        d: "0",
-        correct: "a",
-    },
-    {
-        question: "What does CSS stand for?",
-        a: "Cascading Style Sheet",
-        b: "Costcutting Style SpreadSheet",
-        c: "Champion Style Sheet ",
-        d: "Computer Software System",
-        correct: "a",
-    },
-    {
-        question: "What is H2O element symbol of?",
-        a: "Water",
-        b: "Hydrogen",
-        c: "Oxygen",
-        d: "Nitrogen",
-        correct: "a",
-    },
-    {
-        question: "Who wrote Macbeth?",
-        a: "Stephen King",
-        b: "Stan Lee",
-        c: "JK Rowling",
-        d: "Shakespheare",
-        correct: "d",
-    },
-];
+function createNote() {
 
-const quiz = document.getElementById("quiz");
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const a_text = document.getElementById("atext");
-const b_text = document.getElementById("btext");
-const c_text = document.getElementById("ctext");
-const d_text = document.getElementById("dtext");
-const submitBtn = document.getElementById("submit");
+    //  Creating class attribute
+    const att = document.createAttribute("class");
+    const close = document.createAttribute("id");
 
-let currentQuiz = 0;
-let score = 0;
+    // Set the value of the class attribute:
+    att.value = "box-container";
+    close.value = "hide" + 1;
 
-loadQuiz();
+    //  Creating a new sticky note
+    //  Creating new div, h2, text area
+    const div = document.createElement("DIV");
+    div.setAttributeNode(att, close);
+    const h2 = document.createElement("H2");
+    const text = document.createTextNode("Sticky Note");
+    const textarea = document.createElement("TEXTAREA");
 
-function loadQuiz() {
-    deselectAnswers();
-    
-    const currentQuizData = quizData[currentQuiz];
+    //  Creating close button 
+    const btn = document.createElement("BUTTON");
+    btn.innerText = "Close";
+    btn.onclick = function() {closeNote()}
+    div.appendChild(btn);
+    h2.appendChild(text);
+    div.appendChild(h2);
+    div.appendChild(textarea);
+    document.body.appendChild(div);
 
-    questionEl.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
 }
 
-function deselectAnswers() {
-    answerEls.forEach((answerEl) => {
-        answerEl.checked = false;
-    });
+function closeNote() {
+    document.getElementById('hide').style.display='none';
 }
-
-function getSelected() {
-    let answer = undefined;
-
-    answerEls.forEach((answerEl) => {
-        if (answerEl.checked) {
-            answer = answerEl.id;
-        }
-    });
-
-    return answer;
-}
-
-submitBtn.addEventListener("click", () => {
-    // check to see the answer
-    const answer = getSelected();
-
-    if (answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++;
-        }
-
-        currentQuiz++;
-        if (currentQuiz < quizData.length) {
-            loadQuiz();
-        } else {
-            quiz.innerHTML = `
-                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
-                
-                <button onclick="location.reload()">Reload</button>
-            `;
-        }
-    }
-});
